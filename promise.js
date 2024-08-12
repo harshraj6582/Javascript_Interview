7426296b60b2ccc72a8daf47bbf4cb93589abb36
harshraj6582 <harshraj211430@gmail.com>
2024-08-12 15:38:35 +0530

    Event bublbing

---
+++ promise.js
@@ -0,0 +1,14 @@
+// // A promise is an object that may produce a single 
+// // value some time int he future with the either 
+// // Resolves or th Reject 
+// // 1- Fullfilled 
+// // 2- Rejected 
+// // 3- Pending 
+
+
+// // const promise = new Promise((resolve, reject))=>
+//     // The Issue with the Global and the local variables are 
+// It gets diffcult to debug and test the cdoe relies on the global variables 
+
+
+// The isFinite function is used to determine the whether a number is finite or infinite 
